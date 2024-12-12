891609b974c5e7af4d2dd0f18ab70d1f7ca785b0
ENnyikadzanzwa <nyikadzanzwaemmanuelnyasha@gmail.com>
2024-12-12 12:16:39 +0200

    Update STATIC_ROOT to align with Vercel

--- work_learning_platform/settings.py
+++ work_learning_platform/settings.py
@@ -118,11 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),  # This is for your local static files
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
