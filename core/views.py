ad9edf5624ab4888ef9886b68ef4323b99f005fb
ENnyikadzanzwa <nyikadzanzwaemmanuelnyasha@gmail.com>
2024-12-18 12:08:13 +0200

    good morning

--- core/views.py
+++ core/views.py
@@ -549,7 +549,10 @@ def all_messages_view(request):
     if not request.user.is_authenticated or request.user.extendeduser.role != 'University Admin':
         return redirect('login')
 
-    university = request.user.university
+     try:
+        university = University.objects.get(university_admin=request.user)
+    except University.DoesNotExist:
+        return redirect('login')
     messages = Message.objects.filter(recipient=university).order_by('-sent_at')
 
     return render(request, 'core/university/all_messages.html', {'messages': messages})
